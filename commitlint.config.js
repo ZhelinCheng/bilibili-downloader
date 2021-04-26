3361bacc13fcfad3ae7859adab1b2e551b8db5a9
ZhelinCheng <chengzhelin@foxmail.com>
2021-04-26 10:55:42 +0800

    build: 优化

---
+++ commitlint.config.js
@@ -0,0 +1,46 @@
+/*
+ * @Author       : Zhelin Cheng
+ * @Date         : 2021-04-26 10:47:19
+ * @LastEditors  : Zhelin Cheng
+ * @LastEditTime : 2021-04-26 10:54:31
+ * @FilePath     : /bilibili-downloader/commitlint.config.js
+ * @Description  : 未添加文件描述
+ */
+
+module.exports = {
+  parserPreset: 'conventional-changelog-conventionalcommits',
+  rules: {
+    'body-leading-blank': [1, 'always'],
+    'body-max-line-length': [2, 'always', 100],
+    'footer-leading-blank': [1, 'always'],
+    'footer-max-line-length': [2, 'always', 100],
+    'header-max-length': [2, 'always', 100],
+    'subject-case': [
+      2,
+      'never',
+      ['sentence-case', 'start-case', 'pascal-case', 'upper-case'],
+    ],
+    'subject-empty': [2, 'never'],
+    'subject-full-stop': [2, 'never', '.'],
+    'type-case': [2, 'always', 'lower-case'],
+    'type-empty': [2, 'never'],
+    'type-enum': [
+      2,
+      'always',
+      [
+        'build',
+        'chore',
+        'ci',
+        'docs',
+        'feat',
+        'fix',
+        'perf',
+        'refactor',
+        'revert',
+        'style',
+        'test',
+        'ref',
+      ],
+    ],
+  },
+};
