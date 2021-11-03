ed129a0786c80137131739e7b6d81e069337025a
程哲林 <chengzhelin@foxmail.com>
2021-11-03 22:18:04 +0800

    fix: 优化日志打印

--- src/utils/index.ts
+++ src/utils/index.ts
@@ -2,7 +2,7 @@
  * @Author       : Zhelin Cheng
  * @Date         : 2021-02-18 20:19:40
  * @LastEditors  : Zhelin Cheng
- * @LastEditTime : 2021-11-03 22:07:45
+ * @LastEditTime : 2021-11-03 22:05:53
  * @FilePath     : \bilibili-downloader\src\utils\index.ts
  * @Description  : 未添加文件描述
  */
@@ -13,12 +13,16 @@ export * from './request';
 export * from './database';
 
 export const logger = pino({
-  transport: {
+  /* transport: {
     target: 'pino-pretty',
     options: {
       colorize: true,
       translateTime: 'SYS:standard',
     },
+  }, */
+  prettyPrint: {
+    colorize: true,
+    translateTime: 'SYS:yyyy-mm-dd HH:MM:ss',
   },
 });
 
