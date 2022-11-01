5ae74e8e6299ac9363c271bd32150ffd3d480faa
ZhelinCheng <chengzhelin@foxmail.com>
2022-11-01 19:47:58 +0800

    feat: 优化代码

--- src/utils/index.ts
+++ src/utils/index.ts
@@ -44,67 +44,65 @@ export const readCookie = (): string => {
 };
 
 // 登陆
-export async function login() {
-  const { status, data } = await userInfo();
+export function login() {
+  return new Promise(async (resolve) => {
+    const { status, data } = await userInfo();
 
-  if (status !== 200) {
-    console.error(`接口状态错误：${status}`);
-    return null;
-  }
+    if (status !== 200) {
+      console.error(`接口状态错误：${status}`);
+      resolve(null);
+    } else if (data.code < 0) {
+      // 获取二维码登陆
+      const qc = await getQrCode();
 
-  if (data.code < 0) {
-    // 获取二维码登陆
-    const qc = await getQrCode();
+      if (qc.data.code === 0) {
+        const qcData = qc.data.data;
 
-    if (qc.data.code === 0) {
-      const qcData = qc.data.data;
+        qrcode.generate(qcData.url, { small: true });
+        console.log(`⬆️⬆️⬆️ 请扫码二维码登陆 ⬆️⬆️⬆️`);
 
-      qrcode.generate(qcData.url, { small: true });
-      console.log(`⬆️⬆️⬆️ 请扫码二维码登陆 ⬆️⬆️⬆️`);
+        const timer = setInterval(async () => {
+          const res = await loginStatus(qcData.qrcode_key);
+          const { code, refresh_token, message } = res.data.data;
+          if ([0, 86038].includes(code)) {
+            clearInterval(timer);
+            resolve(null);
+          }
+          if (code === 0) {
+            const setCookie = res.headers['set-cookie'];
 
-      const timer = setInterval(async () => {
-        const res = await loginStatus(qcData.qrcode_key);
-        const { code, refresh_token, message } = res.data.data;
-        if ([0, 86038].includes(code)) {
-          clearInterval(timer);
-        }
-        if (code === 0) {
-          const setCookie = res.headers['set-cookie'];
+            const cookies = setCookie.map((ck: string) => {
+              return ck.split(';')[0];
+            });
 
-          const cookies = setCookie.map((ck: string) => {
-            return ck.split(';')[0];
-          });
+            writeJsonFile({
+              cookie: cookies.join(';'),
+              token: refresh_token,
+            });
 
-          writeJsonFile({
-            cookie: cookies.join(';'),
-            token: refresh_token,
-          });
+            const user = await userInfo();
+            State.vipStatus = user.data.data.vip_status > 0;
+            State.isLogin = true;
+            console.log(
+              `登录成功，是否为大会员：${State.vipStatus ? '是' : '否'}`,
+            );
 
-          const user = await userInfo();
-          State.vipStatus = user.data.data.vip_status > 0;
-          State.isLogin = true;
-          console.log(
-            `登录成功，是否为大会员：${State.vipStatus ? '是' : '否'}`,
-          );
-        } else {
-          console.log(message);
-        }
-      }, 3000);
+            resolve(true);
+          } else {
+            console.log(message);
+          }
+        }, 3000);
+      }
+    } else {
+      State.vipStatus = data.data.vip_status > 0;
+      State.isLogin = true;
+      console.log(`登录成功，是否为大会员：${State.vipStatus ? '是' : '否'}`);
+
+      resolve(true);
     }
-  } else {
-    State.vipStatus = data.data.vip_status > 0;
-    State.isLogin = true;
-    console.log(`登录成功，是否为大会员：${State.vipStatus ? '是' : '否'}`);
 
-    return {
-      is_login: true,
-      ...data.data,
-    };
-  }
-
-  console.error(`登录失败`);
-
-  return null;
+    resolve(null);
+  });
 }
 
 /* export function polling() {
